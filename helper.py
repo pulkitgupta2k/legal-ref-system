4c8eda15c21f7ef45f5baacc699c8632b0f4b498
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-20 20:52:19 +0530

    download complete

--- helper.py
+++ helper.py
@@ -13,5 +13,18 @@ def getHTML(link):
 def getJudgements():
     link = "https://legalref.judiciary.hk/lrs/common/ju/newjudgments.jsp"
     html = str(getHTML(link))
-    doc_links = re.findall("<a\s+(?:[^>]*?\s+)?href=/(.*?) ", html)
-    pprint(doc_links)
+    doc_links = []
+    for doc_link in re.findall("<a\s+(?:[^>]*?\s+)?href=/(.*?) ", html):
+        doc_link = "https://legalref.judiciary.hk/" + doc_link
+        doc_links.append(doc_link)
+    return doc_links
+
+def downloadfile(link):
+    name = "download_files/"+link.split('/')[-1]
+    d_file = requests.get(link)
+    open(name, "wb").write(d_file.content)
+
+def driver():
+    links = getJudgements()
+    for link in links:
+        downloadfile(link)
